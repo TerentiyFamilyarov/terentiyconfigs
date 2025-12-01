75035e675bc4241aded1c6d66116add528865b36
TerentiyFamilyarov <144107285+TerentiyFamilyarov@users.noreply.github.com>
2025-12-01 15:43:53 +0300

    finish line 1

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,8 +28,8 @@ vim.opt.relativenumber = true
 vim.opt.syntax = on
 
 -- Табы и отступы
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 -- Отсутпы прокрутки
